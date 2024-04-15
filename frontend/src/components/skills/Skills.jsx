fd5ce04458addbb4cd10d7fceee9c75f717f1d6a
kunwarrohit <rohitsingh95650@gmail.com>
2024-04-15 06:52:49 +0530

    producton:adding skills

--- frontend/src/components/skills/Skills.jsx
+++ frontend/src/components/skills/Skills.jsx
@@ -7,6 +7,18 @@ const Skills = () => {
       <div className="title1">
         <p className="description">My Skills</p>
       </div>
+      <div>
+        <p>C/C++</p>
+        <p>Python</p>
+        <p>JavaScript</p>
+        <p>React.js</p>
+        <p>Node.js</p>
+        <p>Express.js</p>
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>MongoDB</p>
+        <p>SQL</p>
+      </div>
     </div>
   );
 };
